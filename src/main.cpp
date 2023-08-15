780209643f7c2c5c2498295cecfefcb3782ccaec
Prajak Poonyawatpornkul <pjaun@hotmail.com>
2023-08-15 16:08:16 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+// เริ่มต้นด้วยการโหลด Library ต่าง ๆ ที่ต้องใช้
 #include <Arduino.h>
+#include <TridentTD_LineNotify.h> //สำหรับการส่งข้อความใน LINE Notify
 
-void setup() {
+#define LINE_TOKEN "xxxxxxxxxxxxxxxxxxxxxxxxxx" // ใส่ รหัส TOKEN ที่ได้มาจากข้างบน
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
